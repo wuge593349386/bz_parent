2a2d51e7109cd4e3627cb36699460ece5c836b29
B1la <593349386@qq.com>
2020-03-28 20:55:34 +0800

    【新增】新增

---
+++ backend_item/src/main/java/com/bjsxt/backend/controller/ItemController.java
@@ -0,0 +1,14 @@
+package com.bjsxt.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wuge
+ * @date 2020/3/28 18:01
+ */
+@RestController
+@RequestMapping("/service/item")
+public class ItemController {
+
+}
